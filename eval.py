f2149faf45331c0431afbf2fee04b68d20bab69a
Lex L <LutherCorporate@gmail.com>
2017-05-21 01:06:36 +0800

    Fix "The image generated by eval.py is black" problem.
    before: nan -> relu -> nan
    now   : nan -> relu(custom) -> 0
    
    - added some comments for a better understanding.

--- eval.py
+++ eval.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from __future__ import print_function
 import tensorflow as tf
-from nets import nets_factory
 from preprocessing import preprocessing_factory
 import reader
 import model
@@ -18,6 +17,8 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def main(_):
+
+    # Get image's height and width.
     height = 0
     width = 0
     with open(FLAGS.image_file, 'rb') as img:
@@ -32,28 +33,41 @@ def main(_):
 
     with tf.Graph().as_default():
         with tf.Session().as_default() as sess:
+
+            # Read image data.
             image_preprocessing_fn, _ = preprocessing_factory.get_preprocessing(
                 FLAGS.loss_model,
                 is_training=False)
             image = reader.get_image(FLAGS.image_file, height, width, image_preprocessing_fn)
+
+            # Add batch dimension
             image = tf.expand_dims(image, 0)
+
             generated = model.net(image, training=False)
+            generated = tf.cast(generated, tf.uint8)
+
+            # Remove batch dimension
             generated = tf.squeeze(generated, [0])
+
+            # Restore model variables.
             saver = tf.train.Saver(tf.global_variables(), write_version=tf.train.SaverDef.V1)
             sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+            # Use absolute path
             FLAGS.model_file = os.path.abspath(FLAGS.model_file)
             saver.restore(sess, FLAGS.model_file)
 
-            start_time = time.time()
-            generated = sess.run(generated)
-            generated = tf.cast(generated, tf.uint8)
-            end_time = time.time()
-            tf.logging.info('Elapsed time: %fs' % (end_time - start_time))
+            # Make sure 'generated' directory exists.
             generated_file = 'generated/res.jpg'
             if os.path.exists('generated') is False:
                 os.makedirs('generated')
+
+            # Generate and write image data to file.
             with open(generated_file, 'wb') as img:
+                start_time = time.time()
                 img.write(sess.run(tf.image.encode_jpeg(generated)))
+                end_time = time.time()
+                tf.logging.info('Elapsed time: %fs' % (end_time - start_time))
+
                 tf.logging.info('Done. Please check %s.' % generated_file)
 
 
